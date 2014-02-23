f36ce50be02bae2bae39fcc0afc1c43d998a8bb3
sunshinezxf <sunshinezxf@hotmail.com>
2014-02-23 15:02:17 +0800

    add implement of abstract class VIP

--- src/model/VIP.java
+++ src/model/VIP.java
@@ -1,5 +1,35 @@
 package model;
 
-public class VIP {
+public abstract class VIP {
+	private String code;
+	private double price;
+
+	public VIP() {
+		code = "";
+		price = 0;
+	}
+
+	public VIP(final String code, final double price) {
+		this.code = code;
+		this.price = price;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public abstract void calculatePrice();
 
 }
