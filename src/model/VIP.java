b857d71daa5504431d629abbe2e582628ff94a13
sunshine <sunshinezxf@hotmail.com>
2014-03-01 20:10:59 +0800

    only vip is needed regardless of whether he/she is single or of a home

--- src/model/VIP.java
+++ src/model/VIP.java
@@ -43,4 +43,52 @@ public class VIP {
 		cardList.remove(card);
 		return cardList;
 	}
+	
+	/**
+	 * function register *
+	 * 
+	 * @param name
+	 * @param password
+	 * @return whether the register action is success
+	 */
+	public boolean register(String name, String password) {
+		setName(name);
+		setPassword(password);
+		return true;
+	}
+
+	/**
+	 * function to modify vip information *
+	 * 
+	 * @param name
+	 * @param password
+	 * @return whether the modify action is success
+	 */
+	public boolean modifyInfo(String name, String password) {
+		setName(name);
+		setPassword(password);
+		return true;
+	}
+
+	/**
+	 * function to activate the card *
+	 * 
+	 * @param card
+	 * @param money
+	 * @return whether the activate action is success
+	 */
+	public boolean activate(VIPCard card, double money) {
+		return card.activate(money);
+	}
+
+	/**
+	 * function to pay the rent every month *
+	 * 
+	 * @param card
+	 * @param money
+	 * @return whether the pay action is success
+	 */
+	public boolean pay(VIPCard card, double money) {
+		return card.pay(money);
+	}
 }
