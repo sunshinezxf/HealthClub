efcc537feaec8d985a6a5216f265e5a1dd34cda4
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-01 14:13:35 +0800

    implementation of SingleVIP id and HomeVIP id

--- src/util/IDGenerator.java
+++ src/util/IDGenerator.java
@@ -1,5 +1,21 @@
 package util;
 
 public class IDGenerator {
-	
+	private final int LENGTH = 5;
+
+	public String generateSGVIP() {
+		return "SG" + generateCode();
+	}
+
+	public String generateHMVIP() {
+		return "HM" + generateCode();
+	}
+
+	private String generateCode() {
+		char[] temp = new char[LENGTH];
+		for (int i = 0; i < LENGTH; i++) {
+			temp[i] = Random.generate();
+		}
+		return new String(temp);
+	}
 }
