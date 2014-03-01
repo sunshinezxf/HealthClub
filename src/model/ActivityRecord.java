7871295440681c2454b115d7c2ca20b6d9e56574
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-01 21:55:12 +0800

    add activity record

--- src/model/ActivityRecord.java
+++ src/model/ActivityRecord.java
@@ -1,5 +1,39 @@
 package model;
 
+import java.util.Calendar;
+
 public class ActivityRecord {
-	
+	private Calendar date;
+	private VIP vip;
+	private Place place;
+
+	public ActivityRecord() {
+		date = null;
+		vip = new VIP();
+		place = null;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public VIP getVip() {
+		return vip;
+	}
+
+	public void setVip(VIP vip) {
+		this.vip = vip;
+	}
+
+	public Place getPlace() {
+		return place;
+	}
+
+	public void setPlace(Place place) {
+		this.place = place;
+	}
 }
