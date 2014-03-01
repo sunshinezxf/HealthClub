cc88b55b0b4c8f427f107b49c3dc92eccd8fef01
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-01 15:00:33 +0800

    modify LENGTH to static

--- src/util/IDGenerator.java
+++ src/util/IDGenerator.java
@@ -1,17 +1,17 @@
 package util;
 
 public class IDGenerator {
-	private final int LENGTH = 5;
+	private final static int LENGTH = 5;
 
-	public String generateSGVIP() {
+	public static String generateSGVIP() {
 		return "SG" + generateCode();
 	}
 
-	public String generateHMVIP() {
+	public static String generateHMVIP() {
 		return "HM" + generateCode();
 	}
 
-	private String generateCode() {
+	private static String generateCode() {
 		char[] temp = new char[LENGTH];
 		for (int i = 0; i < LENGTH; i++) {
 			temp[i] = Random.generate();
