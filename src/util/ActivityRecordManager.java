6f69115a03410cc4ef04f4f2fa2a67cd337ad853
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-02 12:49:10 +0800

    activity record manager which is used to get record list of a specified user

---
+++ src/util/ActivityRecordManager.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.ArrayList;
+
+import model.ActivityRecord;
+
+public class ActivityRecordManager {
+	public static ArrayList<ActivityRecord> checkActivityRecord(String username) {
+		return null;
+	}
+}
