093aa7a00c20ed59019084408a8faf123acca9cf
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-02 12:50:14 +0800

    pay record manager which is used to get the record list of a specified user

---
+++ src/util/PayRecordManager.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.ArrayList;
+
+import model.PayRecord;
+
+public class PayRecordManager {
+	public static ArrayList<PayRecord> checkPayRecord(String username) {
+		return null;
+	}
+}
