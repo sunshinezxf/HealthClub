a66a10f412ebad2d2ec2660f3f1cfea230bb2ae0
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-02 13:03:35 +0800

    vip manager acts register and cancel vip

---
+++ src/util/manager/VIPManager.java
@@ -0,0 +1,11 @@
+package util.manager;
+
+public class VIPManager {
+	public static boolean registerVIP(String username, String password) {
+		return true;
+	}
+
+	public static boolean cancelVIP(String username) {
+		return true;
+	}
+}
