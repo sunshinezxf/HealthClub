0ea83ab1e14f59dee39831bcc1350950309560f4
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-03 14:05:12 +0800

    stub method of VIPDAO

--- src/dao/VIPDAO.java
+++ src/dao/VIPDAO.java
@@ -6,15 +6,15 @@ public class VIPDAO {
 	public static boolean addVIP(VIP vip) {
 		return true;
 	}
-	
+
 	public static boolean deleteVIP(String username) {
 		return true;
 	}
-	
+
 	public static VIP find(String username) {
 		return null;
 	}
-	
+
 	public static boolean modifyVIP(VIP vip) {
 		return true;
 	}
