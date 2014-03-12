4830edc293879fb259f26e34b33c143ef7c270d8
sunshinezxf <sunshinezxf@hotmail.com>
2014-03-12 10:58:08 +0800

    add class Show Member action stub

---
+++ src/action/ShowMember.java
@@ -0,0 +1,29 @@
+package action;
+
+import service.AttendantService;
+
+@SuppressWarnings("serial")
+public class ShowMember extends BaseAction {
+	private String a_id;
+	private AttendantService attendantService;
+
+	public AttendantService getAttendantService() {
+		return attendantService;
+	}
+
+	public void setAttendantService(AttendantService attendantService) {
+		this.attendantService = attendantService;
+	}
+
+	public String execute() throws Exception {
+		return null;
+	}
+
+	public String getA_id() {
+		return a_id;
+	}
+
+	public void setA_id(String a_id) {
+		this.a_id = a_id;
+	}
+}
